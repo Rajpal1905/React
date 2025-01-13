58d5f0d695b27015297c1e9f9f616125e5ce9a49
rajpal <raj.palvm19@gmail.com>
2025-01-13 11:58:06 +0530

    commiting blogContext

---
+++ blogContext/src/components/Headers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Headers = () => {
+  return (
+    <div className='w-full border shadow-md py-4 fixed top-0 bg-white'>
+      <header className='text-center'>
+        <h1 className=' text-3xl font-bold uppercase'>CodeHelp Blogs</h1>
+      </header>
+    </div>
+  )
+}
