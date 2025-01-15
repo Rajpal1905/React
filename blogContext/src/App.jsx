dd4bb188439b4a25e3d9629dc97e2c778a8b596b
rajpal <raj.palvm19@gmail.com>
2025-01-15 14:57:16 +0530

    adding new things in blog project

--- blogContext/src/App.jsx
+++ blogContext/src/App.jsx
@@ -1,24 +1,42 @@
 import { useEffect } from "react"
-import { Blogs } from "./components/Blogs"
-import { Headers } from "./components/Headers"
-import { Pagination } from "./components/Pagination"
 import { useContext } from "react"
 import { AppContext } from "./Context/AppContext"
+import { Route, Routes, useLocation, useSearchParams } from "react-router-dom"
+import { Homes } from "./pages/Homes"
+import { BlogPage } from "./pages/BlogPage"
+import { TagPage } from "./pages/TagPage"
+import { CategoryPage } from "./pages/CategoryPage"
 
 
 function App() {
   const { fetchBlogsData } = useContext(AppContext)
 
+  const [searchParams,setSearchParam] = useSearchParams()
+  const location = useLocation()
   
   useEffect(()=>{
-    fetchBlogsData()
-  },[])
+    const page = searchParams.get("page") ?? 1
+
+    if(location.pathname.includes("tags")){
+      const tag = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogsData(Number(page), tag);
+    }
+   else if(location.pathname.includes("categories")){
+      const category = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogsData(Number(page), null , category);
+    }
+    else {
+      fetchBlogsData(Number(page))
+    }
+  },[location.pathname , location.search])
+
   return (
-    <div className='w-full h-full flex flex-col gap-y-1 justify-center items-center '>
-      <Headers />
-      <Blogs />
-      <Pagination />
-    </div>
+   <Routes>
+      <Route path="/" element = {<Homes/>}/>
+      <Route path="/blog/:blogId" element = {<BlogPage/>}/>
+      <Route path="/tags/:tag" element = {<TagPage/>}/>
+      <Route path="/categories/:category" element = {<CategoryPage/>}/>
+   </Routes>
   )
 }
 
--- blogContext/src/components/Headers.jsx
+++ blogContext/src/components/Headers.jsx
@@ -4,7 +4,7 @@ export const Headers = () => {
   return (
     <div className='w-full border shadow-md py-4 fixed top-0 bg-white'>
       <header className='text-center'>
-        <h1 className=' text-3xl font-bold uppercase'>CodeHelp Blogs</h1>
+        <h1 className=' text-3xl font-bold uppercase font-slab tracking-wider '>React-Blogsify</h1>
       </header>
     </div>
   )
